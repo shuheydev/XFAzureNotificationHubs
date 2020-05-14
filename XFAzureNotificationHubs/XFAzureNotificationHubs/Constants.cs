be551c3b473cb9560b4cc2d7f6b2d7042bbd6b5d
Shuhey <shuhey.dev@gmail.com>
2020-05-14 12:52:07 +0900

    ignore Constants.cs

--- XFAzureNotificationHubs/XFAzureNotificationHubs/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace XFAzureNotificationHubs.Droid
-{
-    //Azure Notification Hubを使うための情報はこれだけ.
-    public static class Constants
-    {
-        public const string ListenConnectionString = "<DefaultListenConnectionString from Azure>";
-        public const string NotificationHubName = "<NotificationHub Name from Azure>";
-    }
-}
