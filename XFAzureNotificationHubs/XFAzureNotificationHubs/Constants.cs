d96b2f01f68d839171cb3edaa3d408c4f3352634
Shuhey <shuhey.dev@gmail.com>
2020-05-14 12:55:13 +0900

    Added Constants.cs

---
+++ XFAzureNotificationHubs/XFAzureNotificationHubs/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace XFAzureNotificationHubs.Droid
+{
+    //Azure Notification Hubを使うための情報はこれだけ.
+    public static class Constants
+    {
+        public const string ListenConnectionString = "<DefaultListenConnectionString from Azure>";
+        public const string NotificationHubName = "<NotificationHub Name from Azure>";
+    }
+}
